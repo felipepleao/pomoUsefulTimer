d48559124efd9fc140929c55652e380da52774a3
Felipe Leão <felipeleao.tech@gmail.com>
2024-01-15 22:26:38 -0300

    att

--- scripts/elements.js
+++ scripts/elements.js
@@ -2,6 +2,7 @@ const html = document.querySelector('html')
 const focoBtn = document.querySelector(".time-mode-list-item--foco");
 const curtoBtn = document.querySelector(".time-mode-list-item--descanso-curto");
 const longoBtn = document.querySelector(".time-mode-list-item--descanso-longo");
+// const longoBtn = document.querySelector(".time-mode-list-item--descanso-longo");
 
 export const elements = {
   html,
